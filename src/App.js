9e8d4fc063d6dea8b65f1a3e46c710539e0ad47d
vkig2615 <bharanis683@gmail.com>
2025-03-03 14:54:47 +0530

    v6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello World - Version v5</h1>
+      <h1>Hello World - Version v6</h1>
     </div>
   )
 } 
